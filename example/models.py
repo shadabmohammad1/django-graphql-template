591f4d5b3dd4c0666933171faa8e86a1db2eb43c
Mohammad Shadab <Mohammad.s@greyorange.com>
2022-09-11 20:06:49 +0530

    Pep8 changes

--- example/models.py
+++ example/models.py
@@ -32,7 +32,9 @@ class Book(models.Model):
 class Grocery(models.Model):
     product_tag = models.CharField(max_length=10)
     name = models.CharField(max_length=100)
-    category = models.ForeignKey(Category, related_name="grocery", on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name="grocery", on_delete=models.CASCADE
+    )
     price = models.IntegerField()
     quantity = models.IntegerField()
     imageurl = models.URLField()
